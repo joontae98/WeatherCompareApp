f85acae87d7a502788ead4d2a352a66458c5fe7e
jt.Park <joontae98@gmail.com>
2020-12-28 13:51:15 +0900

    Add: Add getJSON method

--- app/src/main/java/com/example/weathercompareapp/MainActivity.java
+++ app/src/main/java/com/example/weathercompareapp/MainActivity.java
@@ -2,22 +2,89 @@ package com.example.weathercompareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView text;
-    String key = "96d98409169b4ef34c4529ad092f8471";
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class MainActivity extends AppCompatActivity {
+    TextView textviewJSONText;
+    ProgressDialog progressDialog;
+    String result;
+    String key = "96d98409169b4ef34c4529ad092f8471";
+    String lat = "35.2450439";
+    String lon = "129.0189522";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = findViewById(R.id.main_text);
+        Button buttonRequestJSON = (Button) findViewById(R.id.button_main_requestjson);  //button_main_requestjson 연결
+        textviewJSONText = (TextView) findViewById(R.id.textview_main_jsontext);         //textview_main_jsontext 연결
+        textviewJSONText.setMovementMethod(new ScrollingMovementMethod());              //
+
+        buttonRequestJSON.setOnClickListener(new View.OnClickListener() {               //버튼 클릭 시 onClick실행
+            @Override
+            public void onClick(View v) {
+
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Please wait.....");
+                progressDialog.show();
+
+                getJSON();
+            }
+        });
     }
 
+    public void getJSON() {
+        String requestUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key;
+
+        Thread thread = new Thread(new Runnable() { //thread 사용 이유?
+            @Override
+            public void run() {
+                try {
+
+                    Log.d("연결된 url", requestUrl);
+                    URL url = new URL(requestUrl);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                    httpURLConnection.setRequestMethod("GET");
+
+                    InputStream inputStream;
+                    inputStream = httpURLConnection.getInputStream();
 
 
+                    StringBuilder builder = new StringBuilder();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line);
+                    }
+
+                    result = builder.toString();
+                    Log.d("결과", result);
+                    reader.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
 }
